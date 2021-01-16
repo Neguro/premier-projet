da5b5306ebcb83fcffb17b412679981cb8a11d54
Neguro <nassim.ahmedali@gmail.com>
2021-01-16 10:52:37 +0100

    création du premier component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PremierComponent } from './premier/premier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremierComponent
   ],
   imports: [
     BrowserModule,
