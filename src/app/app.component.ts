c0d68b4f6a469baf0fc34e0e63b6040c11ff50bc
Neguro <nassim.ahmedali@gmail.com>
2021-01-17 20:01:55 +0100

    creation du component appareil et test des Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app angular';
+  isAuth = false;
+
+  appareils = [
+    {
+      name: 'Serveur Pegasus',
+      status: false
+    },
+    {
+      name: 'Serveur Leo',
+      status: true
+    },
+    {
+      name: 'Serveur Dragon',
+      status: false
+    }
+  ]
+  constructor()
+  {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
 }
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,43 @@
+import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+  @Input() appareilName: string;
+  @Input() appareilStatus: boolean;
+
+  constructor() 
+  { 
+    this.appareilName = "";
+    this.appareilStatus = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+  getStatus() : string
+  {
+    if(this.appareilStatus === false)
+    {
+      return "éteint";
+    }
+    else
+    {
+      return "allumer";
+    }
+  }
+
+  onAllumer() : boolean
+  {
+    return this.appareilStatus = true;
+  }
+
+  onEteindre() : boolean
+  {
+    return this.appareilStatus = false;
+  }
+}
