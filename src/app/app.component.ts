cb5c70c6c844030b6f2147722bc9db7c86e469e1
Neguro <nassim.ahmedali@gmail.com>
2021-01-19 13:22:52 +0100

    ajout d'un compteur de temps lors du demarrage de la session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+import { Observable } from 'rxjs-compat/Observable';
+import { Subscription } from 'rxjs-compat/Subscription';
+import 'rxjs-compat/add/observable/interval';
+import { AuthService } from './services/auth.service';
+import { values } from 'sequelize/types/lib/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app angular';
-  isAuth = false;
 
+export class AppComponent implements OnInit {
+  title = 'app angular';
+  isAuth: boolean;
+  secondes: number;
+  counterSubscription: Subscription;
+  
   lastUpdate = new Promise((resolve, reject) => {
     const date = new Date();
     setTimeout(
@@ -18,7 +25,47 @@ export class AppComponent {
       }, 2000);
   });
 
-  constructor() {
-   
+  constructor(private authService: AuthService) {
+   this.secondes = 0;
+   this.isAuth = false;
+   this.counterSubscription = new Subscription;
   }
+  
+  ngOnInit() 
+  {
+      const counter = Observable.interval(1000);
+      this.startObservable(counter);   
+  }
+
+  ngOnDestroy()
+  {
+    this.counterSubscription.unsubscribe();
+  }
+  
+  getAuthState() : Boolean
+  {
+    return this.authService.isAuth;
+  }
+
+  startObservable(obs : Observable<number>) 
+  {
+    this.counterSubscription = obs.subscribe(
+      (value) => {
+        if(this.getAuthState())
+        {
+          this.secondes += 1; 
+        }
+        else
+        {
+          this.secondes = 0;
+        }
+      },
+      (error) =>{
+        throw new Error ("Un erreur c'est déclancher");
+      },
+      () => {
+        console.log("Observable terminer")
+      }
+    );
+  } 
 }
