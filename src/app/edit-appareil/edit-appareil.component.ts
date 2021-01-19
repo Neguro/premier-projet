681f144d5407f6c5e119caf43c7cb1ff56d6e112
Neguro <nassim.ahmedali@gmail.com>
2021-01-19 18:34:32 +0100

    fonction onSubmit pour le form terminer

--- src/app/edit-appareil/edit-appareil.component.ts
+++ src/app/edit-appareil/edit-appareil.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router, RouterLink, RouterModule } from '@angular/router';
+import { AppareilService } from '../services/appareil.service';
 
 @Component({
   selector: 'app-edit-appareil',
@@ -8,13 +10,18 @@ import { NgForm } from '@angular/forms';
 })
 export class EditAppareilComponent implements OnInit {
 
-  constructor() { }
+  defaultOnOff: boolean = false;
+  constructor(private appareilService: AppareilService, private router:  Router) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(form: NgForm)
+  onSubmit(form: NgForm) : void
   {
-    console.log(form.value);
+    const name = form.value["name"];
+    const status = form.value["status"];
+    this.appareilService.addAppareil(name,status); 
+    this.router.navigate(['/appareils'])
   }
+  
 }
